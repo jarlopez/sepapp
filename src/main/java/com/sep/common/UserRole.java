3add8ebbacad3559b7055cce1a48ad6891f92113
Johan Mickos <jmickos@bu.edu>
2016-10-15 12:55:19 +0200

    Add new user roles

--- src/main/java/com/sep/common/UserRole.java
+++ src/main/java/com/sep/common/UserRole.java
@@ -1,12 +1,30 @@
 package com.sep.common;
 
 public enum UserRole {
+    // Internal & Testing
     ADMIN,
-    TEAM_MEMBER,
-    TEAM_MANAGER,
+
+    // Administrative Department
+    ADMIN_MANAGER,
+
     CUSTOMER_SERVICE,
     CUSTOMER_SERVICE_MANAGER,
+
+    HR_MEMBER,
+    HR_MANAGER,
+
+    MARKETING_MEMBER,
+    MARKETING_MANAGER,
+
+    // Financial Department
     FINANCIAL_MANAGER,
+    ACCOUNTANT,
+
+    // Production & Services
+    TEAM_MEMBER,
+    TEAM_MANAGER,
+
+    // Upper Management
     VICE_PRESIDENT;
 
     public String roleName() {
