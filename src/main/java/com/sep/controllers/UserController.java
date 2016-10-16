40c946bea723cef61020efcbf26c5a7d7bd4baff
Johan Mickos <jmickos@bu.edu>
2016-10-16 18:23:45 +0200

    Class cleanup

--- src/main/java/com/sep/controllers/UserController.java
+++ src/main/java/com/sep/controllers/UserController.java
@@ -1,53 +1,15 @@
 package com.sep.controllers;
 
-import com.sep.domain.User;
-import com.sep.services.SecurityService;
-import com.sep.services.UserService;
-import com.sep.validator.UserValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
     Logger log = LoggerFactory.getLogger(UserController.class);
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private SecurityService securityService;
-
-    @Autowired
-    private UserValidator userValidator;
-
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registration(Model model) {
-        model.addAttribute("userForm", new User());
-
-        return "registration";
-    }
-
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
-        userValidator.validate(userForm, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "registration";
-        }
-
-        userService.save(userForm);
-
-        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
-
-        return "redirect:/welcome";
-    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
@@ -59,7 +21,6 @@ public class UserController {
             model.addAttribute("message", "You have been logged out successfully.");
             return "login";
         }
-        log.info("Returning login");
         return "login";
     }
 
