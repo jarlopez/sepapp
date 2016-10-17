132434456b3eb0509e01d4d2a3a05423ba314dd0
Johan Mickos <jmickos@bu.edu>
2016-10-17 17:03:37 +0200

    Finalize staffing, status updates'

--- src/main/java/com/sep/controllers/StaffController.java
+++ src/main/java/com/sep/controllers/StaffController.java
@@ -1,14 +1,13 @@
 package com.sep.controllers;
 
 import com.sep.domain.StaffingRequest;
+import com.sep.domain.StaffingRequestStatus;
 import com.sep.repositories.StaffingRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -44,4 +43,52 @@ public class StaffController {
         model.addAttribute("requests", staffingRequestRepository.findAll());
         return "staffing/list";
     }
+
+    @GetMapping("/request/start/{id}")
+    public String start(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        StaffingRequest it = staffingRequestRepository.findOne(id);
+        if (it == null) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request not found");
+            return "redirect:/staff/request/list";
+        }
+        if (it.getStatus() != StaffingRequestStatus.OPEN) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request already started");
+            return "redirect:/staff/request/list";
+        }
+        it.setStatus(StaffingRequestStatus.PENDING);
+        staffingRequestRepository.save(it);
+        return "redirect:/staff/request/list";
+    }
+    @GetMapping("/request/close/{id}")
+    public String close(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        StaffingRequest it = staffingRequestRepository.findOne(id);
+        if (it == null) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request not found");
+            return "redirect:/staff/request/list";
+        }
+        if (it.getStatus() != StaffingRequestStatus.OPEN && it.getStatus() != StaffingRequestStatus.PENDING) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request already started");
+            return "redirect:/staff/request/list";
+        }
+        it.setStatus(StaffingRequestStatus.CLOSED);
+        staffingRequestRepository.save(it);
+        return "redirect:/staff/request/list";
+    }
+    @GetMapping("/request/reject/{id}")
+    public String reject(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        StaffingRequest it = staffingRequestRepository.findOne(id);
+        if (it == null) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request not found");
+            return "redirect:/staff/request/list";
+        }
+        if (it.getStatus() != StaffingRequestStatus.OPEN && it.getStatus() != StaffingRequestStatus.PENDING) {
+            redirectAttributes.addAttribute("error", "Invalid action! Request already started");
+            return "redirect:/staff/request/list";
+        }
+        it.setStatus(StaffingRequestStatus.REJECTED);
+        staffingRequestRepository.save(it);
+        return "redirect:/staff/request/list";
+    }
 }
+
+
